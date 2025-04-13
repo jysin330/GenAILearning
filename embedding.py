c62deaea004739d073679bc53a3b32604dd9ee49
Jyoti Singh <jyoti@mushroom.solutions>
2025-04-14 01:16:42 +0530

    gemini model used

--- embedding.py
+++ embedding.py
@@ -1,12 +1,21 @@
+from google import genai
+from google.genai import types
 from dotenv import load_dotenv
-from openai import OpenAI
 load_dotenv()
+import os
 
-client = OpenAI()
-text = "Effel Tower is in Paris and is a famous landmark, it is 324 meters tall"
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=GEMINI_API_KEY)
 
-response = client.embeddings.create(
-    input=text,  
-    model= "text-embedding-3-small"
-)
-print(f"Response : {response.data[0].embedding}")
+result = client.models.embed_content(
+        model="gemini-embedding-exp-03-07",
+        contents="Effel Tower is in Paris and is a famous landmark, it is 324 meters tall",)
+
+print(result.embeddings)
+
+# response = client.embeddings.create(
+#     input=text,  
+#     model= "text-embedding-3-small"
+# )
+# print(f"Response : {response.data[0].embedding}")
+print(f"Response : {response.text}")
--- tokenization.py
+++ tokenization.py
@@ -1,12 +1,19 @@
-import tiktoken
+# import tiktoken
 
-encoder =tiktoken.encoding_for_model("gpt-4o")
-print(f"encoder n vocab {encoder.n_vocab}") # 2,00,019 --> 200k
+# encoder =tiktoken.encoding_for_model("gpt-4o")
+# print(f"encoder n vocab {encoder.n_vocab}") # 2,00,019 --> 200k
+# text = "The cat Sat On The Mat"
+# tokens = encoder.encode(text)
+# print(f'Tokens : {tokens}') # [976, 19288, 22232, 2160, 623, 9926]
+
+# my_tokens =  [976, 19288, 22232, 2160, 623, 9926]
+
+# decoded = encoder.decode(my_tokens)
+# print(f"Decoded : {decoded}")
+
+from vertexai.preview import tokenization
+encoder = tokenization.get_tokenizer_for_model("gemini-1.5-flash-002")
 text = "The cat Sat On The Mat"
-tokens = encoder.encode(text)
-print(f'Tokens : {tokens}') # [976, 19288, 22232, 2160, 623, 9926]
+tokens_info = encoder.compute_tokens(text).tokens_info
+print(f"encode n vocab", tokens_info[0].token_ids) 
 
-my_tokens =  [976, 19288, 22232, 2160, 623, 9926]
-
-decoded = encoder.decode(my_tokens)
-print(f"Decoded : {decoded}")
